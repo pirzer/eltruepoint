c54d13be0a9d82ab46ca4e3a517d19c6a8c4a4af
pirzer <pirzer1@yahoo.com>
2023-05-08 21:15:50 +0000

    removing line of polls frim settings in install app section

--- eltruepoint/settings.py
+++ eltruepoint/settings.py
@@ -40,7 +40,6 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

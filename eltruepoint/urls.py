343324018146b9b4b5a93128a68f868efbd4133f
pirzer <pirzer1@yahoo.com>
2023-05-08 20:51:01 +0000

    connecting urls blog eltruepoint and polls

--- eltruepoint/urls.py
+++ eltruepoint/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('', include('blog.urls'), name='blog_urls'),
     path('summernote/', include('django_summernote.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
     path('polls/', include('polls.urls')),
 ]
--- polls/views.py
+++ polls/views.py
@@ -38,6 +38,7 @@ class ResultsView(generic.DetailView):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    print("question")
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
